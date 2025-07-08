9f209d907b589d859c8b8155ac46b89933538e3f
Zhaleh Jamalivand <zhaleh.jamalivand@gmail.com>
2025-07-08 19:06:59 +0200

    Refactor P/Invoke signatures in Program.cs to use string parameters for improved clarity and compatibility; update .rustc_info.json files for consistency

--- CharpProjectsUseDLLRust/CsharptCsvParallelBatchProcessorUseRustDll/Program.cs
+++ CharpProjectsUseDLLRust/CsharptCsvParallelBatchProcessorUseRustDll/Program.cs
@@ -11,10 +11,10 @@ class Program
     static extern IntPtr LoadLibrary(string dllToLoad);
 
     [DllImport("rust_csv.dll", CallingConvention = CallingConvention.Cdecl)]
-    private static extern int run_parallel_batch_job(IntPtr inputPath, IntPtr outputPath);
+    private static extern int run_parallel_batch_job(string inputPath, string outputPath);
 
     [DllImport("rust_csv.dll", CallingConvention = CallingConvention.Cdecl)]
-    private static extern int run_parallel_batch_job_for_huge_file(IntPtr inputPath, IntPtr outputPath);
+    private static extern int run_parallel_batch_job_for_huge_file(string inputPath, string outputPath);
 
     static unsafe void Main(string[] args)
     {
@@ -34,16 +34,11 @@ class Program
         long baseMemory = proc.PrivateMemorySize64;
         var stopwatch = Stopwatch.StartNew();
 
-        byte[] inputBytes = Encoding.ASCII.GetBytes(inputPath + "\0");
-        byte[] outputBytes = Encoding.ASCII.GetBytes(outputPath + "\0");
+        // if (inputPath.IndexOf("huge", StringComparison.OrdinalIgnoreCase) >= 0)
+        run_parallel_batch_job_for_huge_file(inputPath, outputPath);
+        // else
+        //     run_parallel_batch_job((IntPtr)pInput, (IntPtr)pOutput);
 
-        fixed (byte* pInput = inputBytes, pOutput = outputBytes)
-        {
-            // if (inputPath.IndexOf("huge", StringComparison.OrdinalIgnoreCase) >= 0)
-            run_parallel_batch_job_for_huge_file((IntPtr)pInput, (IntPtr)pOutput);
-            // else
-            //     run_parallel_batch_job((IntPtr)pInput, (IntPtr)pOutput);
-        }
 
         stopwatch.Stop();
         proc.Refresh();
